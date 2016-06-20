22477a01099b7e3d2e321dd57dc0fcd216871eca
Grace Tsai <gtsai@uci.edu>
2016-06-20 11:53:43 -0700

    previewcardsadded

--- script/javascript.js
+++ script/javascript.js
@@ -7,22 +7,27 @@ var cards = [{
 var cardTitle = document.querySelector('#new-card-title');
 var cardNotes = document.querySelector('#new-card-notes');
 
+
 var element = document.getElementsByClassName("content");
-var child = document.getElementsByClassName("preview-cards");
-var preview = [
-    '<div class="preview_cards">',
-    '    <h3 class="card_title">Title</h3>',
-    '    <div class="author">Author</div>',
-    '    <ul class="card_tags">',
-    '    <li>Science</li>',
-    '    <li>Math</li>',
-    '    <li>History</li>',
-    '    </ul>',
-    '    <div class="card_thumbnail">',
-    '    <img src="images/150x150.jpg" >',
-    '    </div>',
-    '    <p class="upload_date">YYYY-MM-DD</p>'
-].join('');
+
+
+
+function appendPreviewCard(){
+    var preview = `<div class="preview_cards">
+        <h3 class="card_title">${cardTitle.value}</h3>
+        <div class="author">Author</div>
+        <ul class="card_tags">
+        <li>Science</li>
+        <li>Math</li>
+        <li>History</li>
+        </ul>
+        <div class="card_thumbnail">
+        <img src="images/150x150.jpg" >
+        </div>
+        <p class="upload_date">YYYY-MM-DD</p>
+    </div>`;
+    $(element).append(preview);
+};
 
 
 
@@ -40,19 +45,17 @@ $(function(){
 
     $(".save").on('click', function(){
         cards.push({title: cardTitle.value,
-            notes: cardNotes.value})
-        cardTitle.value = "Enter title here"
-        cardNotes.value = "Enter text content here"
-        console.log(cards)
+            notes: cardNotes.value});
+        appendPreviewCard();
+        cardTitle.value = "Enter title here";
+        cardNotes.value = "Enter text content here";
+        console.log(cards);
         fullCardContainer.css("display", "none");
-        
-        element.insertBefore(preview,child);
-        
+
+
+
+
     });
-
-
-
-
 
 
 
