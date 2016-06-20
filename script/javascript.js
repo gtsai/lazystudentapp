5bd7abcf16e3a7e3f7bc59e7be03114f9fd0144e
Grace Tsai <gtsai@uci.edu>
2016-06-20 10:23:45 -0700

    newpreviewcardsstart

--- script/javascript.js
+++ script/javascript.js
@@ -4,8 +4,27 @@ var cards = [{
     notes: 'This is my note'
 }];
 
-var cardTitle = document.querySelector('#new-card-title')
-var cardNotes = document.querySelector('#new-card-notes')
+var cardTitle = document.querySelector('#new-card-title');
+var cardNotes = document.querySelector('#new-card-notes');
+
+var element = document.getElementsByClassName("content");
+var child = document.getElementsByClassName("preview-cards");
+var preview = [
+    '<div class="preview_cards">',
+    '    <h3 class="card_title">Title</h3>',
+    '    <div class="author">Author</div>',
+    '    <ul class="card_tags">',
+    '    <li>Science</li>',
+    '    <li>Math</li>',
+    '    <li>History</li>',
+    '    </ul>',
+    '    <div class="card_thumbnail">',
+    '    <img src="images/150x150.jpg" >',
+    '    </div>',
+    '    <p class="upload_date">YYYY-MM-DD</p>'
+].join('');
+
+
 
 $(function(){
 
@@ -26,7 +45,9 @@ $(function(){
         cardNotes.value = "Enter text content here"
         console.log(cards)
         fullCardContainer.css("display", "none");
-
+        
+        element.insertBefore(preview,child);
+        
     });
 
 
